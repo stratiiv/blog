256790c30e38cd55aaea8cc63b6d254369674c3c
Valentyn Stratii <stratiyvalter@gmail.com>
2022-11-30 18:46:58 +0200

    add auth

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
    path('list/',PostList.as_view(),name='post-list'),
    path('list/<int:pk>/',PostDetail.as_view(),name='post-detail'),
-   path('auth',include('rest_framework.urls'))
+   path('auth/',include('rest_framework.urls')),
+   path('dj-rest-auth/',include('dj_rest_auth.urls')),
+   path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
